15d19fd403e5d2e5b3032f45ca834b6e4444041d
Rick Porter <rickwporter@gmail.com>
2025-04-18 20:32:03 -0400

    Move test to correct directory, and add more

---
+++ tests/cli_gen/test_utils.py
@@ -0,0 +1,46 @@
+import pytest
+
+from oas_tools.cli_gen.utils import maybe_quoted
+from oas_tools.cli_gen.utils import to_camel_case
+from oas_tools.cli_gen.utils import to_snake_case
+
+
+@pytest.mark.parametrize(
+    ["text", "expected"],
+    [
+        pytest.param("foo", "foo", id="unchanged"),
+        pytest.param("fooBar", "foo_bar", id="simple"),
+        pytest.param("snaFooBar", "sna_foo_bar", id="two"),
+        pytest.param("SNAFoo", "sna_foo", id="multi-caps"),
+        pytest.param("sna_foo", "sna_foo", id="snake"),
+    ]
+)
+def test_snake_case(text, expected):
+    assert expected == to_snake_case(text)
+
+
+@pytest.mark.parametrize(
+    ["text", "expected"],
+    [
+        pytest.param("foo", "foo", id="unchanged"),
+        pytest.param("foo_bar", "fooBar", id="simple"),
+        pytest.param("sna_foo_bar", "snaFooBar", id="two"),
+        pytest.param("snaFoo", "snaFoo", id="camel"),
+    ]
+)
+def test_camel_case(text, expected):
+    assert expected == to_camel_case(text)
+
+
+@pytest.mark.parametrize(
+    ["item", "expected"],
+    [
+        pytest.param("foo", '"foo"', id="string"),
+        pytest.param(1, "1", id="int"),
+        pytest.param(None, "None", id="none"),
+        pytest.param(True, "True", id="bool"),
+    ]
+)
+def test_maybe_quoted(item, expected):
+    assert expected == maybe_quoted(item)
+
