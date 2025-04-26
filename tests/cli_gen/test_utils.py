b75cb5766c22e93a6e67c080ce8a6571170ad69d
Rick Porter <rickwporter@gmail.com>
2025-04-26 07:31:44 -0400

    Eliminate redundant testing

--- tests/cli_gen/test_utils.py
+++ tests/cli_gen/test_utils.py
@@ -8,11 +8,13 @@ from oas_tools.cli_gen.utils import to_snake_case
 @pytest.mark.parametrize(
     ["text", "expected"],
     [
+        pytest.param("", "", id="empty"),
         pytest.param("foo", "foo", id="unchanged"),
         pytest.param("fooBar", "foo_bar", id="simple"),
         pytest.param("snaFooBar", "sna_foo_bar", id="two"),
         pytest.param("SNAFoo", "sna_foo", id="multi-caps"),
         pytest.param("sna_foo", "sna_foo", id="snake"),
+        pytest.param("SnaFooBar123_MORE", "sna_foo_bar123_more", id="numbers"),
     ]
 )
 def test_snake_case(text, expected):
@@ -22,10 +24,13 @@ def test_snake_case(text, expected):
 @pytest.mark.parametrize(
     ["text", "expected"],
     [
+        pytest.param("", "", id="empty"),
         pytest.param("foo", "foo", id="unchanged"),
         pytest.param("foo_bar", "fooBar", id="simple"),
+        pytest.param("FooBar", "FooBar", id="unchanged"),
         pytest.param("sna_foo_bar", "snaFooBar", id="two"),
         pytest.param("snaFoo", "snaFoo", id="camel"),
+        pytest.param("sna_foo_bar123_more", "snaFooBar123More", id="numbers"),
     ]
 )
 def test_camel_case(text, expected):
@@ -35,6 +40,7 @@ def test_camel_case(text, expected):
 @pytest.mark.parametrize(
     ["item", "expected"],
     [
+        pytest.param("", "", id="empty"),
         pytest.param("foo", '"foo"', id="string"),
         pytest.param(1, "1", id="int"),
         pytest.param(None, "None", id="none"),
