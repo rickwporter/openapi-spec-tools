6688143a6fb978ff7e6597e595895fa6e8e387cb
Rick Porter <99488405+rickwporter@users.noreply.github.com>
2025-06-21 06:32:27 -0400

    Ignore some more unused-imports (#24)

--- examples/github/github_gen_cli/main.py
+++ examples/github/github_gen_cli/main.py
@@ -8,16 +8,16 @@ from datetime import date  # noqa: F401
 from datetime import datetime  # noqa: F401
 from enum import Enum  # noqa: F401
 from pathlib import Path
-from typing import Optional
-from typing_extensions import Annotated
+from typing import Optional  # noqa: F401
+from typing_extensions import Annotated  # noqa: F401
 
 import typer
 
 from github_gen_cli import _arguments as _a
-from github_gen_cli import _display as _d
-from github_gen_cli import _exceptions as _e
-from github_gen_cli import _logging as _l
-from github_gen_cli import _requests as _r
+from github_gen_cli import _display as _d  # noqa: F401
+from github_gen_cli import _exceptions as _e  # noqa: F401
+from github_gen_cli import _logging as _l  # noqa: F401
+from github_gen_cli import _requests as _r  # noqa: F401
 from github_gen_cli import _tree as _t
 from github_gen_cli.users import app as users
 
