57fb3f8ff61183b82349a2e2fda6a5c87a5ba629
Rick Porter <rickwporter@gmail.com>
2025-07-23 13:01:49 -0400

    CLI commands have lots of options by design

--- openapi_spec_tools/oas.py
+++ openapi_spec_tools/oas.py
@@ -192,7 +192,7 @@ class DisplayOption(str, Enum):
 
 
 @app.command("update", short_help="Update the OpenAPI spec")
-def update(
+def update(  # noqa: PLR0912
     original_filename: OasFilenameArgument,
     updated_filename: Annotated[Optional[str], typer.Option(help="Filename for update OpenAPI spec")] = None,
     nullable_not_required: Annotated[
