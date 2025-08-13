753bdd8d15a5de4434dd31d87a8cc74de2cfa792
Rick Porter <rickwporter@gmail.com>
2025-08-13 10:50:04 -0400

    Add BaseGenerator class and tests

---
+++ openapi_spec_tools/base_gen/constants.py
@@ -0,0 +1,9 @@
+"""Common definitions for code generation."""
+NL = "\n"
+SEP1 = "\n    "
+SEP2 = "\n        "
+
+# map of supported collections to their Python types
+COLLECTIONS = {
+    "array": "list",
+}
