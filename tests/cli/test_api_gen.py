29e9e21e77ae708f2a5f4066cbae4bd9f3b1c393
Rick Porter <rickwporter@gmail.com>
2025-08-20 05:46:52 -0400

    Avoid issue with cwd not existing

--- tests/cli/test_api_gen.py
+++ tests/cli/test_api_gen.py
@@ -13,11 +13,9 @@ from tests.helpers import asset_filename
 
 @pytest.fixture
 def temp_working_dir():
-    orig_dir = os.getcwd()
     with TemporaryDirectory() as temp_dir:
         os.chdir(temp_dir)
         yield temp_dir
-        os.chdir(orig_dir)
 
 
 def _read_text(filename: str) -> str:
